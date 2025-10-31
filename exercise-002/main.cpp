dd83cd480a32a430aab2c97baedacf6b91ed8928
Christian Ege <226893+christianege@users.noreply.github.com>
2025-10-31 15:14:27 +0100

    feat: provide the next step

--- exercise-002/main.cpp
+++ exercise-002/main.cpp
@@ -2,42 +2,17 @@
 #include <cstdlib>
 #include <cstring>
 
-#define SOULS 100
-
-char* cursed() {
-    char *soul = (char*)malloc(8);
-    strcpy(soul, "boo!");
-    // why is this not working?
-    //soul = "test";
-    // but this?
-    //const char* soul2 = "whooo!"; 
-    fmt::println("{} address of soul: {}", soul, fmt::ptr(soul));
-    //fmt::println("{} address of soul2: {}", soul2, fmt::ptr(soul2));
-    // common pitfall what happens here?
-    // fmt::println("{} address of soul: {}", soul, fmt::ptr(&soul));
-    return soul;
-}
-
-void exorcism(char* victim) {
-    if(NULL != victim) {
-        fmt::println("Attenrobendum eos, ad constringendum, ad ligandum eos, pariter et solvendum, et ad congregandum eos coram me, {}", fmt::ptr(victim));
-        free((void*)victim);
-    }
-}
-
 int main() {
-  
-    for(int i=0; i < SOULS; i++) {
-        exorcism(cursed());
-        
-    }
-    const unsigned int max_cursed = 100;
-    char* cursed_souls[max_cursed];
-    for(int i = 0; i < max_cursed; i++) {
-        cursed_souls[i] = cursed();
-    }
-    for(int i = 0; i < max_cursed; i++) {
-        exorcism(cursed_souls[i]);
-        cursed_souls[i] = NULL;
-    }
+    char *ghost = (char*)malloc(8);
+    strcpy(ghost, "evil");
+    fmt::println("The value of ghost: {}", ghost);
+    free(ghost);
+    // use after free
+    fmt::println("{}", ghost);
+    strcpy(ghost, "mad");
+    fmt::println("{}", ghost);
+    char* trap = (char*)malloc(8);
+    strcpy(trap, "caught");
+    fmt::println("trap: {}", trap);
+    fmt::println("ghost: {}", ghost);
 }
